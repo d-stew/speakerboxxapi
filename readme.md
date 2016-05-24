966d749d76637b9202a7927e3d87a6352f6a33a5
Anthony Simpson <ansi6622@colorado.edu>
2016-05-24 10:35:32 -0600

    adding validation

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ### Create_db
 createdb speakeasy_db
+create .env inside root of directory, put the example.env stuff inside
 npm install
 knex migrate:latest
 knex seed:run
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
-
+var knex = require('../db/knex');
+var bcrypt = require('bcrypt');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json('respond with a resource');
+});
+
+router.post('/signup', function(req, res, next) {
+  const errors = []
+
+  if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
+  if (!req.body.name || !req.body.name.trim()) errors.push("Name can't be blank");
+  if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
+
+  if (errors.length) {
+    console.log(errors);
+    res.status(422).json({
+      errors: errors
+    })
+  } else {
+
+    knex('users')
+      .whereRaw('lower(email) = ?', req.body.email.toLowerCase())
+      .count()
+      .first()
+      .then(function (result) {
+         if (result.count === "0") {
+           const saltRounds = 4;
+           const passwordHash = bcrypt.hashSync(req.body.password, saltRounds);
+
+           var data = {
+             name: req.body.name,
+             email: req.body.email,
+             password: passwordHash,
+           }
+
+           knex('users').insert(data)
+             .returning('*')
+             .then(function (users) {
+               console.log(users);
+               res.redirect('/twitter')
+             })
+
+         } else {
+           return knex('users')
+            .whereRaw('lower(email) = ?', req.body.email.toLowerCase())
+            .first()
+            .then(function (result) {
+              if (result) {
+                var validPassword = bcrypt.compareSync(req.body.password, result.password)
+                if (validPassword) {
+                  res.redirect('/twitter')
+                }
+              } else {
+                res.status(422).json({
+                  errors: ["Invalid email"]
+                })
+              }
+          })
+        }
+      })
+  }
 });
 
 module.exports = router;
